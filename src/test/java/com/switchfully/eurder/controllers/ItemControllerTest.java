a53dc999d4d7b3b7c08134e4f563a40e83459d79
Jonas Natadiredja <jonas.natadiredja@gmail.com>
2022-11-29 16:41:00 +0100

    Story 2: Add an item - tests

--- src/test/java/com/switchfully/eurder/controllers/ItemControllerTest.java
+++ src/test/java/com/switchfully/eurder/controllers/ItemControllerTest.java
@@ -3,26 +3,27 @@ package com.switchfully.eurder.controllers;
 import com.switchfully.eurder.domain.Address;
 import com.switchfully.eurder.domain.Role;
 import com.switchfully.eurder.domain.User;
-import com.switchfully.eurder.dto.CreateCustomerDTO;
 import com.switchfully.eurder.dto.CreateItemDTO;
-import com.switchfully.eurder.dto.CustomerDTO;
 import com.switchfully.eurder.dto.ItemDTO;
+import com.switchfully.eurder.repositories.UserRepository;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import java.lang.reflect.Member;
 
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ItemControllerTest {
+    @Autowired
+    private UserRepository userRepository;
     private User admin;
+    private User customer;
+
     @LocalServerPort
     private int port;
 
@@ -37,13 +38,25 @@ class ItemControllerTest {
                 "0498416686",
                 Role.ADMIN
         );
+
+        customer = new User(
+                "Frederik",
+                "Sok",
+                "frederik@eurder.com",
+                "password",
+                new Address("Teststraat", "50", "2000", "Antwerp"),
+                "0498416686",
+                Role.CUSTOMER
+        );
+
+        userRepository.create(customer);
     }
 
     @Test
     void givenARequestBody_whenAddingANewItem_httpStatusCreatedAndNewItemReturned() {
         CreateItemDTO itemToAdd = new CreateItemDTO(
                 "name",
-                "testItem2",
+                "testItem",
                 6.0,
                 30
         );
@@ -65,7 +78,7 @@ class ItemControllerTest {
     void givenARequestBodyWithMissingName_whenAddingNewItem_httpStatusBadRequest() {
         CreateItemDTO itemToAdd = new CreateItemDTO(
                 "",
-                "testItem2",
+                "testItem",
                 6.0,
                 30
         );
@@ -81,7 +94,7 @@ class ItemControllerTest {
     void givenRequestHeaderWithIncorrectAuthorization_whenAddingItem_throwsUnauthorizedException() {
         CreateItemDTO itemToAdd = new CreateItemDTO(
                 "name",
-                "testItem2",
+                "testItem",
                 6.0,
                 30
         );
@@ -89,6 +102,22 @@ class ItemControllerTest {
         RestAssured
                 .given().contentType(JSON).body(itemToAdd).accept(JSON)
                 .auth().preemptive().basic(admin.getEmailAddress(), "invalidPassword")
+                .when().port(port).post("/items")
+                .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN);
+    }
+
+    @Test
+    void givenCustomer_whenAddingItem_throwsUnauthorizedException() {
+        CreateItemDTO itemToAdd = new CreateItemDTO(
+                "name",
+                "testItem",
+                6.0,
+                30
+        );
+
+        RestAssured
+                .given().contentType(JSON).body(itemToAdd).accept(JSON)
+                .auth().preemptive().basic(customer.getEmailAddress(), customer.getPassword())
                 .when().port(port).post("/items")
                 .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN);
     }
